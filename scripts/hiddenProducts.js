9707ab853fd037ecb4ea1afbe3fa47becdc81049
Ramiro Melero <ramiromelero@gmail.com>
2021-11-29 11:41:27 -0300

    Update hiddenProducts.js

--- scripts/hiddenProducts.js
+++ scripts/hiddenProducts.js
@@ -1,3 +1,13 @@
-function hiddenFunction() {
+let productsToHide = document.querySelector(".container_right");
+let enterOpen = document.querySelector(".desktop_btn");
+let enterClose = document.querySelector(".desktop_btn");
+
+enterOpen.addEventListener("click", hideFunction);
+enterClose.addEventListener("dblclick", showFunction);
+
+function hideFunction() {
     document.getElementById("container_right").style.visibility = "hidden";
-  }
+}
+function showFunction() {
+    document.getElementById("container_right").style.visibility = "visible"
+}
